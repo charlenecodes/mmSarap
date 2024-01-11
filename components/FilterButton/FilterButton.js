2b85961fc9a0bdc20babcf51d69cbfb6e28e7394
charlenecodes <cjlava17@yahoo.com>
2024-01-11 15:26:12 +0100

    improve recipe filter

--- components/FilterButton/FilterButton.js
+++ components/FilterButton/FilterButton.js
@@ -1,6 +1,7 @@
 import {StyleSheet, Text, Pressable, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 export default function FilterButton({cuisine, setCuisine}) {
   // returns the different cuisines
@@ -13,53 +14,57 @@ export default function FilterButton({cuisine, setCuisine}) {
 
   useEffect(() => {
     async function getCuisines() {
-      // ^ somehow when the user clicks on one of the selection, the cuisine variable needs to be updated so the backend will search for recipes only under a certain category - if a filter is selected make sure to have a remove filter button to reset
-      if (localhost) {
-        try {
-          await axios
-            .get(`http://${localhost}:3000/recipes/cuisines`)
-            .then(res => setCuisines(res.data));
-        } catch (err) {
-          console.error({error: err.message});
-        }
+      try {
+        await axios
+          .get(`http://${localhost}:3000/recipes/cuisines`)
+          .then(res => setCuisines(res.data));
+      } catch (err) {
+        console.error({error: err.message});
       }
     }
     getCuisines();
   }, []);
   return (
     <View style={styles.container}>
-      {cuisines.map((cuisine, index) => {
-        return (
-          <Pressable
-            key={index}
-            //^ how to only trigger one
-            onPress={() => {
-              // ^ what is the best way to print?
-              if (index <= cuisines.length) setCuisine(cuisine);
-            }}
-            // ^ HOW TO MAKE THE STYLING ADAPT TO WHETHER IT WAS CLICKED OR NOT
-            style={cuisine !== null ? styles.button : styles.buttonSelected}>
-            <Text
-              // try to customize it so that when the button is pressed it will have a different style
-              style={cuisine !== null ? styles.text : styles.textSelected}>
-              {cuisine}
-            </Text>
-          </Pressable>
-        );
-      })}
       {cuisine && (
         <Pressable
+          // ^ HOW TO MAKE THE STYLING ADAPT TO WHETHER IT WAS CLICKED OR NOT
+          style={styles.buttonSelected}
           onPress={() => {
             setCuisine(null);
-          }}
-          style={styles.resetButton}>
-          <Text
-            // try to customize it so that when the button is pressed it will have a different style
-            style={styles.resetText}>
-            Reset Filter
-          </Text>
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginRight: 3,
+            }}>
+            <Text
+              // try to customize it so that when the button is pressed it will have a different style
+              style={styles.textSelected}>
+              {cuisine}
+            </Text>
+            <AntDesign name={'close'} color={'white'} />
+          </View>
         </Pressable>
       )}
+      {cuisine === null &&
+        cuisines.map((cuisine, index) => {
+          return (
+            <Pressable
+              key={index}
+              //^ how to only trigger one
+              onPress={() => {
+                // ^ what is the best way to print?
+                if (index <= cuisines.length) setCuisine(cuisine);
+              }}
+              // ^ HOW TO MAKE THE STYLING ADAPT TO WHETHER IT WAS CLICKED OR NOT
+              style={styles.button}>
+              <Text style={styles.text}>{cuisine}</Text>
+            </Pressable>
+          );
+        })}
     </View>
   );
 }
